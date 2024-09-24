2879a69e8dac7d07b97211ebec0f9c94f76e19ab
Somyakant Swain <130567791+SomyakantSwain@users.noreply.github.com>
2024-09-24 17:40:50 +0530

    Add files via upload

---
+++ Heightandage.py
@@ -0,0 +1,9 @@
+h=int(input("Enter your height"))
+age=int(input("Enter your age"))
+if (h>=3  and age<18):
+    print(" You have to pay 250 rupees")
+elif(h>3 and age>=18):
+    print(" You have to pay 500 rupees")
+
+else: "you are not allowed"
+print("Thank you")
